4906298a1a9f6a676475e207e91c76bcb10af0cc
Dennis Quan <dquan@alum.mit.edu>
2022-11-16 07:16:20 -0500

    start of lecture 23 css example: scrollable demo

---
+++ lecture23-css/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="primary">
+      <b-navbar-brand href="#">
+        CSS Demo
+      </b-navbar-brand>
+      <b-navbar-nav>
+        <b-nav-item href="/scrollable">Scrollable</b-nav-item>
+      </b-navbar-nav>
+    </b-navbar>
+    <router-view />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref, provide } from 'vue'
+</script>
---
+++ lecture23-css/src/views/Scrollable.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="mx-3 my-3">
+    <div style="overflow-y: scroll; height: 50vh">
+      <span v-for="i in 100">
+        The quick brown fox jumps over the lazy dog
+      </span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+</script>

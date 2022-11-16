03e871111ea07a03b7f22e706bb0202c357f43cd
Dennis Quan <dquan@alum.mit.edu>
2022-11-16 10:38:24 -0500

    theme modal

--- lecture23-css/src/App.vue
+++ lecture23-css/src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <b-navbar toggleable="lg" type="dark" variant="primary">
+    <b-navbar toggleable="lg" type="dark" :variant="navBarVariant">
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-      <b-navbar-brand href="#">
+      <b-navbar-brand v-b-modal.theme-modal href="#">
         CSS Demo
       </b-navbar-brand>
       
@@ -16,10 +16,32 @@
       </b-collapse>
     </b-navbar>
 
+    <b-modal id="theme-modal" @cancel="handleCancel" title="Theme">
+      <b-form-radio-group
+        v-model="navBarVariant"
+        :options="[
+          { text: 'Blue', value: 'primary' },
+          { text: 'Red', value: 'danger' },
+        ]"
+      />
+    </b-modal>
+
     <router-view />
   </div>
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref, provide } from 'vue'
+import { BvModalEvent } from 'bootstrap-vue'
+import { getCurrentInstance, ref } from 'vue'
+
+// an unfortunate but necessary hack to allow 
+// $-style BootstrapVue APIs to be called from Vue 2.7 and above
+const { $bvModal } = (getCurrentInstance()!.proxy) as any
+
+const navBarVariant = ref("primary")
+
+function handleCancel(bvModalEvent: BvModalEvent) {
+  bvModalEvent.preventDefault()
+  $bvModal.msgBoxOk("Sorry, we didn't implement cancel correctly!")
+}
 </script>

3b46c525d3f9944796fb38cae36e3dff89a0173d
Dennis Quan <dquan@alum.mit.edu>
2022-11-16 09:21:36 -0500

    responsiveness improvements

--- lecture23-css/src/App.vue
+++ lecture23-css/src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <div>
     <b-navbar toggleable="lg" type="dark" variant="primary">
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
       <b-navbar-brand href="#">
         CSS Demo
       </b-navbar-brand>
-      <b-navbar-nav>
-        <b-nav-item href="/scrollable">Scrollable</b-nav-item>
-        <b-nav-item href="/positioning">Positioning</b-nav-item>
-        <b-nav-item href="/grid">Grid</b-nav-item>
-      </b-navbar-nav>
+      
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="/scrollable">Scrollable</b-nav-item>
+          <b-nav-item href="/positioning">Positioning</b-nav-item>
+          <b-nav-item href="/grid">Grid</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
     </b-navbar>
+
     <router-view />
   </div>
 </template>
--- lecture23-css/src/views/Grid.vue
+++ lecture23-css/src/views/Grid.vue
@@ -2,24 +2,24 @@
   <div class="mx-3 my-3">
     <b-container fluid>
       <b-row>
-        <b-col md="12" lg="3" class="border">
+        <b-col sm="12" md="3" class="border">
           1
         </b-col>
-        <b-col md="12" lg="6" class="border">
+        <b-col sm="12" md="6" class="border">
           2
         </b-col>
-        <b-col md="12" lg="3" class="border">
+        <b-col sm="12" md="3" class="border">
           3
         </b-col>
       </b-row>
       <b-row>
-        <b-col md="12" lg="7" class="border">
+        <b-col sm="12" md="7" class="border">
           4
         </b-col>
-        <b-col md="12" lg="2" class="border">
+        <b-col sm="12" md="2" class="border">
           5
         </b-col>
-        <b-col md="12" lg="3" class="border">
+        <b-col sm="12" md="3" class="border">
           6
         </b-col>
       </b-row>
